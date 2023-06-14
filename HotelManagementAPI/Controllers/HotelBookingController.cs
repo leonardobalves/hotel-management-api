39efe64c2fc4726bcb8f626ea1db319a784bd3ab
leonardobalves <leonardobuckalves@gmail.com>
2023-06-13 21:55:27 -0300

    add create/edit method in controller

--- HotelManagementAPI/Controllers/HotelBookingController.cs
+++ HotelManagementAPI/Controllers/HotelBookingController.cs
@@ -14,5 +14,29 @@ namespace HotelManagementAPI.Controllers
         {
             _context = context;
         }
+
+        [HttpPost]
+        public JsonResult CreateEdit(HotelBooking booking)
+        {
+            if (booking.Id == 0)
+            {
+                _context.Bookings.Add(booking);
+            }
+            else
+            {
+                var bookingInDb = _context.Bookings.Find(booking.Id);
+
+                if (bookingInDb == null)
+                {
+                    return new JsonResult(NotFound());
+                }
+
+                bookingInDb = booking;
+            }
+
+            _context.SaveChanges();
+
+            return new JsonResult(Ok(booking));
+        }
     }
 }
