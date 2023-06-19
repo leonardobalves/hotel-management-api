e0bb4c60f511429303da0b486728db870ec970a7
leonardobalves <leonardobuckalves@gmail.com>
2023-06-19 20:32:48 -0300

    create booking controller

---
+++ HotelManagementAPI/Controllers/Bookings.cs
@@ -0,0 +1,18 @@
+﻿using HotelManagementAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagementAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class Bookings : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public Bookings(ApiContext context)
+        {
+            _context = context;
+        }
+    }
+}
